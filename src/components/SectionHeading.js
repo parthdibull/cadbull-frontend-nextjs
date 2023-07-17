2954e93fd458bf90a3088cbd1c0ceba4356610c6
pankajsyal1 <pankaj.syal1@gmail.com>
2023-07-17 18:33:00 +0530

    project of the day card

--- src/components/SectionHeading.js
+++ src/components/SectionHeading.js
@@ -1,8 +1,8 @@
 import HeadingImage from "@/assets/images/heading-image.png"
-const SectionHeading = ({ subHeading, mainHeading, mainHeadingBold }) => {
+const SectionHeading = ({ subHeading, mainHeading, mainHeadingBold, alignment }) => {
 
   return (
-    <div className="position-relative mx-auto d-inline-block">
+    <div className={`position-relative d-inline-block ${alignment}`}>
       <h6 className="d-inline-block mb-2">{subHeading}</h6>
       <div className="heading-image-wrapper"> <img src={HeadingImage.src} alt="headingImage" className="img-fluid" /></div>
       <h3>{mainHeading} <span>{mainHeadingBold}</span></h3>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,9 @@ import { Fragment } from 'react'
 import SectionHeading from '@/components/SectionHeading'
 import Link from 'next/link'
 import Icons from '@/components/Icons';
-import img from "@/assets/icons/drawing-type-1.png"
+import img from "@/assets/icons/drawing-type-1.png";
+import drawing1 from "@/assets/images/drawing-image.png";
+import paid from "@/assets/icons/paid.png";
 
 
 
@@ -26,6 +28,7 @@ export default function Home() {
         <meta name="description" content="World Largest 2d CAD Library." />
       </Head>
 
+      {/* Hero section  */}
       <section className='hero py-3 py-md-5'>
         <div className="container py-3 py-md-5">
           <div className="row">
@@ -69,13 +72,69 @@ export default function Home() {
       </section>
 
 
-
+      {/* Project of the Day  */}
       <section className='py-5'>
         <div className="container">
-          <div className="row">
+          <div className="row mb-4 mb-md-5">
             <div className="col-md-12">
-              <div className='d-flex w-100'>
-                <SectionHeading subHeading={"FIND LATEST"} mainHeading={"Cadbull"} mainHeadingBold={"Categories"} />
+              <div className='d-flex justify-content-between align-items-center'>
+                <div>
+                  <SectionHeading subHeading={"DISCOVER"} mainHeading={"Project of the day"} mainHeadingBold={"Cadbull"} alignment="" />
+                </div>
+                <div className='w-100'>
+                  <div className='d-flex gap-3 justify-content-end'>
+                    <form>
+                      <div class="input-group">
+                        <span class="input-group-text bg-white" id="basic-addon1">
+                          <Icons.Search />
+                        </span>
+                        <input type="text" class="form-control  border-start-0 border-end-0 rounded-end-0 ps-0" placeholder="For e.g. House Design" aria-label="For e.g. House Design" />
+                        <span class="input-group-text p-0" id="basic-addon2">
+                          <button type='submit' className='btn btn-secondary rounded-start-0'>SEARCH</button>
+                        </span>
+                      </div>
+                    </form>
+                    {/* Sort by : DWG */}
+                    <div class="d-flex">
+                      <span class="input-group-text bg-white border-end-0 rounded-end-0" id="basic-addon1">
+                        Sort by :
+                      </span>
+                      <select class="form-select border-start-0  rounded-start-0" aria-label=".form-select-sm example">
+                        <option selected>DWG</option>
+                        <option value="1">BIM</option>
+                        <option value="2">SKP</option>
+                        <option value="3">MAX</option>
+                        <option value="3">3DS</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-md-4">
+              <div className='project-day-card'>
+                <div className='project-day-card-image mb-3'>
+                  <img src={drawing1.src} alt="" className='w-100 img-fluid' />
+                </div>
+                <div className='project-day-card-description d-flex justify-content-between'>
+                  <p className='ps-2'>Ground Floor Bathroom Details And Dimension Autocad Fil</p>
+                  <div>
+                    <span className='badge bg-secondary text-white'>DWG</span>
+                    <div className='d-flex gap-1 mt-1 align-items-center'>
+                      <span><Icons.Eye /></span>
+                      <span>369</span>
+                    </div>
+                  </div>
+                </div>
+                <div className='text-end mt-2'>
+                  <img src={paid.src} alt="icon" className='img-fluid' />
+                </div>
+                <div className='project-day-card-link'>
+                  <p>MORE DETAILS</p>
+                </div>
               </div>
             </div>
           </div>
